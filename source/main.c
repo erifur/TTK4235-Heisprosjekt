ca3b57ce765ed15c0b7042a902542f412a1f1bd2
erikrf <erikrf@stud.ntnu.no>
2020-02-17 20:54:58 +0100

    Added (some) hardware reads. Added FSM ideas/blueprint (comments).

--- source/main.c
+++ source/main.c
@@ -7,36 +7,80 @@
 #include "queue.h"
 
 typedef enum {
-    ELEVATOR_IDLE,
-    ELEVATOR_AT_FLOOR,
-    ELEVATOR_MOVING,
-    ELEVATOR_STOPPED
+    ELEVATOR_IDLE,      // Waiting for Orders
+    ELEVATOR_AT_FLOOR,  // Door open at floor
+    ELEVATOR_MOVING,    // Moving to request
+    ELEVATOR_STOPPED    // Stop button pressed
 } ElevatorState;
 
-static ElevatorState elevator_state = ELEVATOR_IDLE;
-
-
+ElevatorState elevator_state = ELEVATOR_IDLE; // Ingen ordre
 
 int main(){
     
+    int elevator_floor; // Current (last known) elevator floor
+    int next_request;   // The next floor request in the queue
+    
+    hardware_init();
     
     while(1){
-        // READING:
-
+        // READING FROM HARDWARE:
+        for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+            // Check internal orders
+            if(hardware_read_order(f, HARDWARE_ORDER_INSIDE)){
+                queue_set_request(f, QUEUE_ORDER_INSIDE);
+                hardware_command_order_light(f, HARDWARE_ORDER_INSIDE, 1);
+            }
+            // Check orders up
+            if(hardware_read_order(f, HARDWARE_ORDER_UP)){
+                queue_set_request(f, QUEUE_ORDER_UP);
+                hardware_command_order_light(f, HARDWARE_ORDER_UP, 1);
+            }
+            // Check orders down
+            if(hardware_read_order(f, HARDWARE_ORDER_DOWN)){
+                queue_set_request(f, QUEUE_ORDER_DOWN);
+                hardware_command_order_light(f, HARDWARE_ORDER_DOWN, 1);
+            }
+            // Check floor
+            if(hardware_read_floor_sensor(f)){
+                hardware_command_floor_indicator_on(f);
+                elevator_floor = f;
+            }
+        
+        }
         
         
-        // WRITING (FSM):
+        // CONTROLLING HARDWARE (FSM):
+        
+        next_request = queue_read_next();
+        
         switch(elevator_state){
             case ELEVATOR_IDLE :
-                
-            case ELEVATOR_AT_FLOOR :
-            
+                // Action:
+                    // ??
+                // Transition:
+                    // if request at same floor --> STOPPED
+                    // if request at diff floor --> MOVING
             case ELEVATOR_MOVING :
-            
+            // Action:
+                // move
+            // Transition:
+                // if reached floor with request --> STOPPED
+            case ELEVATOR_AT_FLOOR :
+            // Action:
+                // wait for timer end, then:
+                // close door, look for new orders
+            // Transition:
+                // always goto IDLE, or directly to new request?
             case ELEVATOR_STOPPED :
+            // Action:
+                // remove all requests,
+                // wait until 3 seconds after stop button released,
+                // don't take requests
+            // Transition:
+                // goto IDLE, or directly to new request?
             
-            
-            
-        }
-    }
-}
+        } // End switch()
+        
+        
+    } // End while()
+} // End main()
