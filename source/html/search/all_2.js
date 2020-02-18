41ab18f23c59f71c31fd5d33fd1024e3b9b44d49
Oliver K. Husby <oliverkh@stud.ntnu.no>
2020-02-18 16:44:16 +0100

    new html

--- source/html/search/all_2.js
+++ source/html/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['timer_21',['timer',['../timer_8h.html#a4b71468423236a99e4efe6c58f6ff10b',1,'timer.c']]],
-  ['timer_2eh_22',['timer.h',['../timer_8h.html',1,'']]]
+  ['timer_2eh_21',['timer.h',['../timer_8h.html',1,'']]],
+  ['timer_5fstart_22',['timer_start',['../timer_8h.html#af2edcf2d8d89ef68e5e7fba5860aeaac',1,'timer.c']]]
 ];
--- source/html/search/functions_2.js
+++ source/html/search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['timer_41',['timer',['../timer_8h.html#a4b71468423236a99e4efe6c58f6ff10b',1,'timer.c']]]
+  ['timer_5fstart_41',['timer_start',['../timer_8h.html#af2edcf2d8d89ef68e5e7fba5860aeaac',1,'timer.c']]]
 ];
