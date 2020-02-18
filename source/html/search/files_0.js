3f2ab26d77214a311ebb2dbd998d68a03648fc58
Oliver K. Husby <oliverkh@stud.ntnu.no>
2020-02-18 13:15:21 +0100

    added timer functionality

---
+++ source/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hardware_2eh_15',['hardware.h',['../hardware_8h.html',1,'']]]
+];
---
+++ source/html/search/files_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['timer_2eh_16',['timer.h',['../timer_8h.html',1,'']]]
+];
