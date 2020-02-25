d6655b64e96b68fc4223b44964807c497c5aa763
Espen Hoegstedt <espen_bh@hotmail.com>
2020-02-25 17:59:25 +0100

    changed variable names

--- source/timer.c
+++ source/timer.c
@@ -1,21 +1,22 @@
 #include "timer.h"
+#include <stdio.h>
 
-static time_t p_start;
-static time_t p_now;
-double timer_length = 3;
+static time_t start;
+static time_t now;
+double timer_length = 3.00;
 
 void timer_start(){
 
-    time(&p_start);   
+    time(&start);   
 
  }
  
  bool is_timer_finished(){
-    time(&p_now);
+    time(&now);
 
-    if(difftime(p_now,p_start)<timer_length)
+    if(difftime(now,start)<timer_length)
     {
-        time(&p_now);
+        time(&now);
         return 0;
     }
     else
--- source/timer.h
+++ source/timer.h
@@ -10,8 +10,7 @@
 
 /**
 *@brief sets the current time to the start-time for the timer
-*@param[in] p_start is set to current time at call of function
-**@warning this function is using a pointer that is dynamically allocated
+*@param start is set to current time at call of function
 */
 void timer_start();
 
@@ -19,10 +18,9 @@ void timer_start();
 *@brief calculates the difference in time between the start time
 *set by {@link timer_start()}
 *@param timer_length the time between current time and "now"-time
-*@param[in] p_start points to the value set by {@link timer_start(time_t* p_start)}
-*@param[in] p_now is continually set to current time
-*@return if the difference between \p p_now and \p p_start is less than \p timer_length it will return 0,
-* else it will free the \p p_now and \p p_start and return 1
-*@warning this function is using pointers that are dynamically allocated
+*@param start is the value set by {@link timer_start(time_t start)}
+*@param now is continually set to current time
+*@return if the difference between \p now and \p start is less than \p timer_length it will return 0,
+* else it will return 1
 */
  bool is_timer_finished();
